cbdcec59dc28632df6f45b26e281fa62697c943d
Kristofer Knutsen <Knutsen@dsl-hkibng42-5673c4-221.dhcp.inet.fi>
2018-04-12 00:04:12 +0300

    First push for the project

---
+++ app/src/main/java/com/example/knutsen/steemy/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.knutsen.steemy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView cabin_View = (ImageView) findViewById(R.id.cabinView);
+        cabin_View.setImageResource(R.drawable.cabin);
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent homeIntent = new Intent(MainActivity. this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+
+
+    }
+
+
+}
